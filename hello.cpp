025ed5f4b120e5d65b78574522f7b858fd9c6daf
Simeon Vincent <svincent@gmail.com>
2012-08-27 21:32:17 -0700

    Removes unecessary code from hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,39 +1,14 @@
 #include <iostream>
-#include <vector>
 #include "game.h"
 using namespace std;
 
 int main()
 {
-  cout << "Hello world!" << endl;
-
   Game myGame;
   myGame.DrawBoard();
-  
-  int x = 7, y = 4;
-  Position pos = 5;
-  
-  while (true) {
-    //cout << "X: ";
-    //if (!(cin >> x))
-    //  break;
-    //cout << "Y: ";
-    //if (!(cin >> y))
-    //  break;
-    //if (myGame.CoordinatesToPosition(x, y, pos))
-    //  cout << ( int ) pos << endl << endl;
-    //else
-    //  cout << "ERROR: Invalid coords" << endl;
 
-    cout << "Position: ";
-    int input;
-    if (!(cin >> input))
-      break;
-    pos = ( Position ) input;
-    if (myGame.PositionToCoordinate(pos, x, y))
-      cout << "Position " << ( int ) pos << " is at coordinate " << x << "," << y << endl << endl;
-    else
-      cout << "ERROR: Position " << ( int ) pos << " is not a valid coodinate." << endl << endl;
-  }
+  bool foo;
+  cin >> foo;
+
   return 0;
 }
